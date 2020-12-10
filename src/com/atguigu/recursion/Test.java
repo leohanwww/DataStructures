b496d792af4bc492340a2911408ea5ee166fd842
leohanwww <leohanwww@gmail.com>
2020-12-10 08:45:53 +0800

    递归

---
+++ src/com/atguigu/recursion/Test.java
@@ -0,0 +1,27 @@
+package com.atguigu.recursion;
+
+
+public class Test {
+    public static void main(String[] args) {
+        //test(4);
+        int res = factorial(3);
+        System.out.println("res="+res);
+    }
+
+    //打印问题
+    public static void test(int n) {
+        if (n > 2) {
+            test(n - 1);
+        }
+        System.out.println("n=" + n);
+    }
+
+    //阶乘问题
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+           return factorial(n - 1) * n;
+        }
+    }
+}
